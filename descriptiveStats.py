d5617e028c4a8b063d204573097eb350c0222fcb
Corin Thummel <thummelcorin@gmail.com>
2020-09-28 13:41:29 -0600

    added general ks test for test statistics

--- descriptiveStats.py
+++ descriptiveStats.py
@@ -244,13 +244,13 @@ def ageVectorStats(probandAgeVector, siblingAgeVector, outputPrefix):
     motherHolm = statsmodels.stats.multitest.multipletests(motherPvalues, alpha=0.05, method='holm', is_sorted=False, returnsorted=False)  
     motherFDR = statsmodels.stats.multitest.fdrcorrection(motherPvalues, alpha=0.05, method='indep', is_sorted=False)
 
-    print("Father Chisq Test Stat test:", stats.ks_2samp(fatherTestStats, "chi2"))
-    print("Father Norm Test Stat test:", stats.ks_2samp(fatherTestStats, "norm"))
-    print("Father Expon Test Stat test:", stats.ks_2samp(fatherTestStats, "expon"))
+    print("Father Chisq Test Stat test:", stats.kstest(fatherTestStats, "chi2"))
+    print("Father Norm Test Stat test:", stats.kstest(fatherTestStats, "norm"))
+    print("Father Expon Test Stat test:", stats.kstest(fatherTestStats, "expon"))
 
-    print("Mother Chisq Test Stat test:", stats.ks_2samp(motherTestStats, "chi2"))
-    print("Mother Norm Test Stat test:", stats.ks_2samp(motherTestStats, "norm"))
-    print("Mother Expon Test Stat test:", stats.ks_2samp(motherTestStats, "expon"))
+    print("Mother Chisq Test Stat test:", stats.kstest(motherTestStats, "chi2"))
+    print("Mother Norm Test Stat test:", stats.kstest(motherTestStats, "norm"))
+    print("Mother Expon Test Stat test:", stats.kstest(motherTestStats, "expon"))
     
     print("Father Mann Test Stat test:", stats.mannwhitneyu(fatherTestStats, motherTestStats))
 
